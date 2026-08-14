20daa1e15c809fc4d464ca2a7d15747a1ea4e370
luanV1n1c1us <netovalmir740@gmail.com>
2026-08-14 18:52:15 -0300

    Exercício 1

---
+++ Aula2/Exercicio1/codigo1/Pessoa.js
@@ -0,0 +1,18 @@
+class Pessoa
+{
+ // Utilizando sintaxe resumida...
+
+ constructor(name, yersOld) {
+    this.name = name;
+    this.yersOld = yersOld
+
+ };
+ apresentar(){
+    // validação para impedir inserção de dados errados
+    if((typeof(this.name) === "string") && (typeof(this.yersOld) === "number") ) {
+        console.log(`Olá, me chamo ${this.name} e tenho ${this.yersOld} ano de idade.`)
+    } 
+ }
+};
+// Exportando a classe que eu
+module.exports = Pessoa;
---
+++ Aula2/Exercicio1/codigo1/app.js
@@ -0,0 +1,7 @@
+const Pessoa = require('./Pessoa.js');
+
+const Person1 = new Pessoa("Maria", 20);
+const Person2 = new Pessoa("João", 20);
+
+Person1.apresentar()
+Person2.apresentar()
