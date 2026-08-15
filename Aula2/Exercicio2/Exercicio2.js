af88176b15b23c7f6673adc87c5caa529959f6c4
luanV1n1c1us <netovalmir740@gmail.com>
2026-08-15 20:24:40 -0300

    Exercicio 2

---
+++ Aula2/Exercicio2/Exercicio2.js
@@ -0,0 +1,6 @@
+/*
+Crie uma classe Carro em Carro.js com atributos modelo, ano e
+velocidade. Implemente os métodos acelerar() (aumenta 10 km/h)
+e frear() (diminui 5 km/h). No app.js, simule uma corrida de 3
+acelerações e 1 freada.
+*/
