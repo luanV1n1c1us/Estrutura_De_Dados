ec1ea914b423fcb3d59cdfb833743af59ad7fa73
luanV1n1c1us <netovalmir740@gmail.com>
2026-08-15 19:30:17 -0300

    Adicionando imagens para o readme

---
+++ Aula2/Exercicio1/Exercicio1.js
@@ -0,0 +1,4 @@
+/*Enunciado Exercício 1 */
+/*Crie uma classe Pessoa em Pessoa.js com os atributos nome e idade e
+um método apresentar(). No app.js, importe a classe, crie dois
+objetos e chame o método para cada um. */
